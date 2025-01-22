0cc740661830b4ca4d225ab101f9d1bc66bb8b53
naaadang <naaadang@naver.com>
2025-01-22 15:22:51 +0900

    DB 연결 및 테스트코드 작성

--- database/chroma_manager.py
+++ database/chroma_manager.py
@@ -2,7 +2,7 @@ import chromadb
 from chromadb.config import Settings
 import os
 from dotenv import load_dotenv
-
+from sentence_transformers import SentenceTransformer
 load_dotenv()
 
 # EC2 퍼블릭 IP와 ChromaDB 포트
@@ -11,7 +11,9 @@ PORT = os.environ.get("CHROMADB_PORT")
 
 
 client = chromadb.HttpClient(host=HOST, port=PORT)
+collection = client.get_or_create_collection(name="library_collection")
 collections = client.list_collections()
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 print("현재 존재하는 컬렉션 목록:")
 for collection_name in collections:
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from app.utils.logger import setup_logger
-from app.modules.search import get_answer
+# from app.modules.search import get_answer
+from app.modules.save_chroma import load_data_to_chromadb
+from app.modules.generate_chroma import generate_response
 import pandas as pd
 
-# Load the FAQ data
-faq_data = pd.read_csv("database/raw/로욜라컬렉션_추천_신착자료.csv")
-
-def main():
-    print("도서관 챗봇에 오신 것을 환영합니다!")
-    print("질문을 입력해 주세요. 종료하려면 'exit'를 입력하세요.")
+# def main():
+#     print("도서관 챗봇에 오신 것을 환영합니다!")
+#     print("질문을 입력해 주세요. 종료하려면 'exit'를 입력하세요.")
     
-    while True:
-        # Get user input
-        user_query = input("\n[사용자 질문] > ")
+#     while True:
+#         # Get user input
+#         user_query = input("\n[사용자 질문] > ")
         
-        # Exit condition
-        if user_query.lower() == "exit":
-            print("챗봇을 종료합니다. 이용해 주셔서 감사합니다!")
-            break
+#         # Exit condition
+#         if user_query.lower() == "exit":
+#             print("챗봇을 종료합니다. 이용해 주셔서 감사합니다!")
+#             break
         
-        # Get the answer
-        answer = get_answer(faq_data, user_query)
-        print(f"[챗봇 답변] > {answer}")
+#         # Get the answer
+#         answer = get_answer(faq_data, user_query)
+#         print(f"[챗봇 답변] > {answer}")
 
 if __name__ == "__main__":
-    main()
+    #main()
+    #load_data_to_chromadb()
+    query=input("질문을 입력하세요: ")
+    response=generate_response(query)
+    print("챗봇 응답: ", response)
